2a238443f57feddd00bf21ed6d40706bd5e74d50
Nemanja Zbiljić <nemanja.zbiljic@gmail.com>
2024-12-02 21:54:20 +0100

    feat(llm): add OpenAI provider implementation

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/coreos/go-semver v0.3.1
 	github.com/duke-git/lancet/v2 v2.3.3
 	github.com/mattn/go-isatty v0.0.20
+	github.com/sashabaranov/go-openai v1.36.0
 	github.com/spf13/cobra v1.8.1
 	github.com/thediveo/enumflag/v2 v2.0.5
 	github.com/tidwall/gjson v1.18.0
@@ -22,4 +23,6 @@ require (
 	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
 	golang.org/x/net v0.27.0 // indirect
 	golang.org/x/term v0.22.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 )
---
+++ pkg/llm/provider/openai.go
@@ -0,0 +1,134 @@
+package provider
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/carlmjohnson/requests"
+	"github.com/duke-git/lancet/v2/slice"
+	"github.com/sashabaranov/go-openai"
+	"github.com/tidwall/gjson"
+
+	"github.com/zbiljic/kai/pkg/llm"
+)
+
+const (
+	openaiBaseURL = "https://api.openai.com/v1/chat/completions"
+	openaiModel   = openai.GPT4oMini
+)
+
+// Compile-time proof of interface implementation.
+var _ llm.AIPrompt = (*OpenAI)(nil)
+
+type OpenAIOptions struct {
+	ApiKey  string
+	BaseURL string
+	Model   string
+}
+
+type OpenAI struct {
+	options OpenAIOptions
+}
+
+func NewOpenAIProvider(opts ...OpenAIOptions) llm.AIPrompt {
+	o := OpenAIOptions{}
+
+	if len(opts) > 0 {
+		o = opts[0]
+	}
+
+	if o.BaseURL == "" {
+		o.BaseURL = openaiBaseURL
+	}
+	if o.Model == "" {
+		o.Model = openaiModel
+	}
+
+	return &OpenAI{
+		options: o,
+	}
+}
+
+func (p *OpenAI) Generate(ctx context.Context, systemPrompt, userPrompt string) ([]string, error) {
+	payload := openai.ChatCompletionRequest{
+		Model: p.options.Model,
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleSystem,
+				Content: systemPrompt,
+			},
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: userPrompt,
+			},
+		},
+		Temperature:      0.7,
+		TopP:             1,
+		FrequencyPenalty: 0,
+		PresencePenalty:  0,
+		MaxTokens:        256,
+		Stream:           false,
+		N:                1,
+	}
+
+	var (
+		respContent openai.ChatCompletionResponse
+		respError   openai.ErrorResponse
+	)
+
+	err := requests.
+		URL(p.options.BaseURL).
+		Post().
+		Headers(map[string][]string{
+			"Authorization": {fmt.Sprintf("Bearer %s", p.options.ApiKey)},
+		}).
+		BodyJSON(payload).
+		ToJSON(&respContent).
+		ErrorJSON(&respError).
+		Fetch(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if respError.Error != nil && respError.Error.Message != "" {
+		return nil, errors.New(respError.Error.Message)
+	}
+
+	if len(respContent.Choices) == 0 {
+		return nil, errors.New("No completion choice available")
+	}
+
+	// extract messages
+	messages := slice.Map(respContent.Choices, func(_ int, s openai.ChatCompletionChoice) string {
+		return s.Message.Content
+	})
+
+	// remove duplicates
+	messages = slice.Unique(messages)
+
+	return messages, nil
+}
+
+func (p *OpenAI) parseLine(line string) (string, error) {
+	data := strings.TrimPrefix(line, "data: ")
+
+	if val := gjson.Get(data, "choices.0.delta.content"); val.Exists() && val.Type == gjson.String {
+		return val.String(), nil
+	}
+
+	return "", nil
+}
+
+func (p *OpenAI) parseStreamResponse(responseText string) (string, error) {
+	text := ""
+	for _, line := range strings.Split(responseText, "\n") {
+		if parsedLine, err := p.parseLine(line); err != nil {
+			return "", err
+		} else {
+			text += parsedLine
+		}
+	}
+	return text, nil
+}
